c7d30cef46f9580667d9499575a32cb5ca443eab
Aadhar Kulshrestha <deepakulshrestha13@gmail.com>
2022-09-29 10:37:44 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,33 +7,34 @@ app.use(express.static("public"));
 const { v4: uuidv4 } = require("uuid");
 
 const io = require("socket.io")(server, {
-    cors: {
-        origin: '*'
-    }
+  cors: {
+    origin: "*",
+  },
 });
 
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
-    debug: true,
+  debug: true,
 });
 
 app.use("/peerjs", peerServer);
 
 app.get("/", (req, res) => {
-    res.redirect(`/${uuidv4()}`);
+  res.redirect(`/${uuidv4()}`);
 });
 
 app.get("/:room", (req, res) => {
-    res.render("index", { roomId: req.params.room });
+  res.render("index", { roomId: req.params.room });
 });
 
 io.on("connection", (socket) => {
-    socket.on("join-room", (roomId, userId, userName) => {
-        socket.join(roomId);
-        socket.on("message", (message) => {
-            io.to(roomId).emit("createMessage", message, userName);
-        });
+  socket.on("join-room", (roomId, userId, userName) => {
+    socket.join(roomId);
+    io.to(roomId).emit("user-connected", userId);
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName);
     });
+  });
 });
 
 server.listen(process.env.PORT || 3030);
